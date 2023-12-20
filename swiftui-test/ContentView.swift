9a6bb319aedcd2044ac5bdaaf822f544b524a880
haneyume <6504021+haneyume@users.noreply.github.com>
2023-12-20 14:06:52 +0800

    Initial Commit

---
+++ swiftui-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-test
+//
+//  Created by Haneyume on 2023/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-test/swiftui_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_testApp.swift
+//  swiftui-test
+//
+//  Created by Haneyume on 2023/12/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
